f980f79595fde319e52d19bde861e34e5467ce71
黑羽快斗 <73578702+kaixin-sourse@users.noreply.github.com>
2022-07-02 15:32:35 +0800

    Create 染色法求二分图.cpp

---
+++ 图论/染色法求二分图.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+const int N = 100010,M = 200010;
+int h[N],e[M],ne[M],idx;
+int n,m;
+int color[N];
+
+void add(int a,int b) {
+    e[idx] = b, ne[idx] = h[a], h[a] = idx ++;
+}
+
+bool dfs(int u,int c) {
+    color[u] = c;
+    for(int i = h[u];~i; i = ne[i]) {
+        int j = e[i];
+        if(!color[j]) {
+            if(!dfs(j,3 - c)) {
+                return false;
+            }
+        } else if(color[j] == color[u]) return false;
+    }
+    return true;
+}
+
+int main() {
+    memset(h,-1,sizeof h);
+    cin >> n >> m;
+    while(m --) {
+        int a,b;
+        cin >> a >> b;
+        add(a,b),add(b,a);
+    }
+    bool flag = true;
+    for(int i = 1; i <= n; i ++) {
+        if(!color[i]) {
+            if(!dfs(i,1)) {
+                flag = false;
+                break;
+            }
+        }
+    }
+    if(flag) puts("Yes");
+    else puts("No");
+    
+    return 0;
+    
+}
